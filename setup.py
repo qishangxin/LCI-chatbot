2c90d27e126154ba515530da8a39fad0abf5e4da
SQ <78365727+qishangxin@users.noreply.github.com>
2023-04-24 22:54:26 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='LCI-chatbot',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='qishangxin',
+    author_email='',
+    description=''
+)
